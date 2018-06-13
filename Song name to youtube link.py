b32964d6a473b825667b60e17d0412542108682c
Walter-Escobar <drkaranmannan@gmail.com>
2018-06-13 23:37:32 +0530

    Update Song name to youtube link.py

--- Song name to youtube link.py
+++ Song name to youtube link.py
@@ -3,14 +3,14 @@ import requests
 import urllib
 
 
-def milo():
+def milo(): #Milo is the name of my dog :P
 
        User_input = input("Title of Youtube vid - ")
 
        words = User_input.split()
 
-       lst = []
-
+       lst = []    #Couldnt figure out a way to "add" words together, so worked my way around.
+ 
        lst.append('+'.join(words))
        print("https://www.youtube.com/results?search_query=" + lst[0])
 
@@ -20,17 +20,13 @@ def milo():
        source_code = requests.get(url)
        plain_text = source_code.text
        soup = BeautifulSoup(plain_text, 'html.parser')
-
-       for link in soup.findAll('a', {'class': 'yt-uix-tile-link'}):
+     
+       for link in soup.findAll('a', {'class': 'yt-uix-tile-link'}): #Had troubles finding the classname.
            lst.append('https://www.youtube.com' + link.get('href'))
-       print(lst[1])
+       print(lst[1]) # Probably much better ways to do this, but this was my work around.
 
 
-# the commented part wouldnt work
-      # for link in soup.findAll('a',"video-title"):
-           # song = link.get('href')
-          # lst.append('https://www.youtube.com' + song)
-          # print(lst[])
+
 
 
 
